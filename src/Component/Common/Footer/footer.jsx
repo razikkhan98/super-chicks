036f6e2080ee9d01cbc9a7fcc49810de547e672a
razikkhan98 <razik.khan@skillintelligence.tech>
2024-03-16 16:41:02 +0530

    working footer Common

---
+++ src/Component/Common/Footer/footer.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+
+// Image
+import Logo from "../../asset/img/Logo/logo.png";
+import FooterImg from "../../asset/img/Footer/footer-img.png";
+
+// React Icon
+import { MdLocationPin } from "react-icons/md";
+import { FaWhatsapp } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <section>
+      {/* <!-- Footer --> */}
+      <div>
+        <img src={FooterImg} alt="Loading" className="img-fluid" />
+      </div>
+      <footer class="text-center text-lg-start background-color-footer">
+        {/* <!-- Grid container --> */}
+        <div class="container pb-0">
+          {/* <!-- Section: Links --> */}
+          <section class="">
+            {/* <!--Grid row--> */}
+            <div class="row">
+              {/* <!-- Grid column --> */}
+              <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 font-weight-bold">
+                  <img
+                    src={Logo}
+                    alt="Loading"
+                    className="img-fluid footer-logo"
+                  />
+                </h6>
+                <p className="text-color-gray">
+                  <span>SuperChicks </span>
+                  delivers fresh & premium meat everyday.We provides you fresh
+                  and hygienic meat poducts at very reasonable price
+                </p>
+              </div>
+              {/* <!-- Grid column --> */}
+
+              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="mb-4 text-color-red">Address</h5>
+                <div className="footer my-2">
+                  <MdLocationPin className="footer-icon" />
+                  <span>Eastern Ring Road Khajrana, Indore</span>
+                </div>
+
+                <div className="footer my-2">
+                  <FaWhatsapp className="footer-icon" />
+                  <span>+91-9244276667</span>
+                </div>
+                <div className="footer my-2">
+                  <MdEmail className="footer-icon" />
+                  <span>info@superchicks.online</span>
+                </div>
+              </div>
+              {/* <!-- Grid column --> */}
+
+              {/* <!-- Grid column --> */}
+              <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="mb-4 text-color-red text-decoration-none">
+                  Important Links
+                </h5>
+                <p>
+                  <a class="text-color-gray text-decoration-none" href="/">
+                    Home
+                  </a>
+                </p>
+                <p>
+                  <a class="text-color-gray text-decoration-none" href="/">
+                    ContactUs
+                  </a>
+                </p>
+                <p>
+                  <a class="text-color-gray text-decoration-none" href="/">
+                    About
+                  </a>
+                </p>
+                <p>
+                  <a class="text-color-gray text-decoration-none" href="/">
+                    Account
+                  </a>
+                </p>
+              </div>
+
+              {/* <!-- Grid column --> */}
+              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="mb-4 text-color-red text-decoration-none">
+                  Social Media
+                </h5>
+
+                <div className="social-media-icon ">
+                  {/* <!-- Facebook --> */}
+                  <a href="#!">
+                    <FaFacebookF className="footer-icon" />
+                  </a>
+
+                  {/* <!-- Twitter --> */}
+                  <a href="#!">
+                    <FaTwitter className="footer-icon" />
+                  </a>
+
+                  {/* <!-- Instagram --> */}
+                  <a href="#!">
+                    <FaInstagram className="footer-icon" />
+                  </a>
+                </div>
+              </div>
+                {/* <hr className="my-4" /> */}
+                <div className="hr"></div>
+            </div>
+            {/* <!--Grid row--> */}
+          </section>
+          {/* <!-- Section: Links --> */}
+        </div>
+
+        {/* <!-- Grid container --> */}
+      </footer>
+      {/* <!-- Footer --> */}
+    </section>
+  );
+};
+
+export default Footer;
--- src/Component/Pages/Home/home.jsx
+++ src/Component/Pages/Home/home.jsx
@@ -4,13 +4,23 @@ import React from "react";
 import TopContent from "../../Common/TopContent/topContent";
 import Categories from "../../Common/Categories";
 import Card from "../../Common/Card/card";
+import Footer from "../../Common/Footer/footer";
+import About from "../About/about";
+
+
 
 // Image
 import HomeIMG from "../../asset/img/Home/image1.png";
-import About from "../About/about";
 import Products1 from "../../asset/img/Products/products-1.png";
 import Products2 from "../../asset/img/Products/products-2.png";
 import Products3 from "../../asset/img/Products/products-3.png";
+import HalalImage1 from "../../asset/img/HalalVerified/Halal-Verified-1.png";
+import HalalImage2 from "../../asset/img/HalalVerified/Halal-Verified-2.png";
+import HalalImage3 from "../../asset/img/HalalVerified/Halal-Verified-3.png";
+import HalalImage4 from "../../asset/img/HalalVerified/Halal-Verified-4.png";
+import HalalImage5 from "../../asset/img/HalalVerified/Halal-Verified-5.png";
+
+
 
 const Home = () => {
   const CardData = [
@@ -36,22 +46,61 @@ const Home = () => {
       button: "Order Now",
     },
   ];
+
+  const HalalImg = [
+    {
+      image: HalalImage1,
+      title: "Halal",
+      title2: "VERIFIED",
+    },
+    {
+      image: HalalImage2,
+      title: "FREE OF",
+      title2: "PRESERVATIVES",
+    },
+    {
+      image: HalalImage3,
+      title: "DELIVERED",
+      title2: "FRESH",
+    },
+    {
+      image: HalalImage4,
+      title: "FSSAI",
+      title2: "REGISTERED",
+    },
+    {
+      image: HalalImage5,
+      title: "FAIR",
+      title2: "PRICING",
+    },
+  ];
+
   return (
     <>
+      {/* Top Content Start */}
       <TopContent
         title=" We Deliver Fresh & Premium Meats Everyday."
         description="Why leave the house? Chicken delivery coming through."
         button="Order Now"
         img={HomeIMG}
       />
+      {/* Top Content End */}
+
       <div className="back-img-main">
+        {/* About Start */}
         <About />
+        {/* About End */}
+
+        {/* Categories Start */}
         <Categories />
+        {/* Categories End */}
+
+        {/* Products Start */}
         <section id="section-padding">
           <div className="container">
             <div className="heading-section mb-5">
               <h3>
-                <span className="">Most Popular</span> Products
+                <span>Most Popular</span> Products
               </h3>
             </div>
 
@@ -59,13 +108,73 @@ const Home = () => {
               {CardData.map((link, index) => (
                 <>
                   <div className="col-lg-4 col-md-6 col-sm-12 my-2">
-                    <Card  image={link.image} title={link.title} kilogram={link.kilogram} amount={link.amount} button={link.button}/>
+                    <Card
+                      image={link.image}
+                      title={link.title}
+                      kilogram={link.kilogram}
+                      amount={link.amount}
+                      button={link.button}
+                    />
                   </div>
                 </>
               ))}
             </div>
           </div>
         </section>
+        {/* Products End */}
+
+        {/* Halal Verified Start */}
+        <section id="section-padding">
+          <div className="container">
+            <div className="heading-section mb-5">
+              <h3>
+                <span> Why order from</span> SuperChicks?
+              </h3>
+              <p className="pb-5">
+                We deliver halal verified, free of preservative , fssai
+                registered, always fresh with fair pricing
+              </p>
+            </div>
+            <div className="d-flex justify-content-between">
+              {HalalImg.map((link, index) => (
+                <div>
+                  <img
+                    src={link.image}
+                    alt="Loading"
+                    className="img-fluid halal-img"
+                    key={index}
+                  />
+                  <div className="text-center pe-3 pt-2 text-uppercase">
+                    {link.title}
+                  </div>
+                  <div className="text-center pe-3 text-uppercase">
+                    {link.title2}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+        {/* Halal Verified End */}
+
+        {/* Customer Start */}
+        <section id="section-padding">
+          <div className="heading-section mb-5">
+            <h3>
+              <span>Hear from our </span>
+            </h3>
+            <h3>Happy Customers</h3>
+
+            {/* This Section Continue Shumaila */}
+          </div>
+        </section>
+        {/* Customer End */}
+
+        {/* Footer Start */}
+
+        <Footer/>
+
+        {/* Footer End  */}
       </div>
     </>
   );
